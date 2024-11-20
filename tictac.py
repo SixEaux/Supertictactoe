0eee26260f7860a38ba985540f68815e96584174
SixEaux <dc200421@gmail.com>
2024-11-20 17:44:53 +0100

    branche principale

--- tictac.py
+++ tictac.py
@@ -5,26 +5,34 @@ class Jeu:
         self.root = root
         self.root.title("SuperTicTacToe")
         self.root.geometry(geometria)
-        self.obj = {} # tous les ojets graphiques
+        self.frames = {} # tous les ojets graphiques de frames avec en cle la position dans le tableau (1,1) , (1,2) ...
         self.tableau = {} # code du tableau c'est 11,12,13,14...
-        self.croix = []
+        self.obj = {i : [] for i in range(9)}
+        self.croix = [] #positions avec croix
         self.rond = [] #positions ou il y a un rond
+        self.positionspossibles = [(i,j) for i in range(3) for j in range(3) if i+j<=4]
         self.initialisationgraph()
 
 
+
+
     def initialisationgraph(self): #creer frames pour chaque garnd carré puis grid
-        c = 0
-        for p in [(i,j) for i in range(3) for j in range(3) if i+j<=4]:
-            f = Frame(self.root, background="black")
-            print(f)
-            self.tableau[c] = {}
-            self.obj[p] = f
+        for p in range(len(self.positionspossibles)):
+            f = Frame(self.root, background="white")
+            self.tableau[p] = {}
+            self.frames[p] = f
+            f.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1])
+        print(self.frames)
 
-            f.grid(row=p[0], column=p[1])
-            c+=1
+        for h in range(9):
+            self.creertictac(h)
 
-    def creertictac(self, frm):
-        pass
+    def creertictac(self, numpos):
+        for p in range(len(self.positionspossibles)):
+            b = Button(self.frames[numpos], borderwidth = 0, background="white", foreground="black")
+            b.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1])
+            self.obj[numpos].append((p, b))
+
 
     def ptitictac(self):
         pass
