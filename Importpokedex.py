0958ae1552afab90ee2b969b809eeb9753faf16f
SixEaux <dc200421@gmail.com>
2024-11-20 10:29:08 +0100

    branche principale

--- Importpokedex.py
+++ Importpokedex.py
@@ -9,4 +9,5 @@ data2["NationalNumber"] = data2["NationalNumber"].apply(lambda x: int(x[1:]))
 diet = pd.merge(data1[data1["Generation"] == 1] , data2, left_on = "Name", right_on = 'Pokemon', how = 'inner').drop(["Generation", "Pokemon", "NationalNumber"], axis = 1)
 print(diet.info())
 
-diet.to_csv("pokedexbien.csv", index = False)
+# diet.to_csv("pokedexbien.csv", index = False)
+
--- tictac.py
+++ tictac.py
@@ -1,24 +1,30 @@
 from tkinter import *
 class Jeu:
     def __init__(self, root, geometria):
-        self.grosfram = None
+
         self.root = root
         self.root.title("SuperTicTacToe")
         self.root.geometry(geometria)
-
-        self.tableau = {j : {i for i in range(9)} for j in range(9)} # code du tableau c'est 11,12,13,14...
-        self.croi = []
+        self.obj = {} # tous les ojets graphiques
+        self.tableau = {} # code du tableau c'est 11,12,13,14...
+        self.croix = []
         self.rond = [] #positions ou il y a un rond
         self.initialisationgraph()
 
 
     def initialisationgraph(self): #creer frames pour chaque garnd carré puis grid
-        self.grosfram = []
+        c = 0
         for p in [(i,j) for i in range(3) for j in range(3) if i+j<=4]:
             f = Frame(self.root, background="black")
-            self.grosfram.append((f, p[0], p[1]))
+            print(f)
+            self.tableau[c] = {}
+            self.obj[p] = f
+
             f.grid(row=p[0], column=p[1])
-        
+            c+=1
+
+    def creertictac(self, frm):
+        pass
 
     def ptitictac(self):
         pass
@@ -26,7 +32,8 @@ class Jeu:
     def placement(self, figure, pos):
         pass
 
-
+    def creerxo(self, signe):
+        pass
 
 
 root = Tk()
