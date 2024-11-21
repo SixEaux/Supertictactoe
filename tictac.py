9354cf6cecc8c3de80bbeffb5c52129fb3596fa4
SixEaux <dc200421@gmail.com>
2024-11-21 10:59:01 +0100

    branche principale

--- tictac.py
+++ tictac.py
@@ -3,6 +3,7 @@ class Jeu:
     def __init__(self, root, geometria):
 
         self.root = root
+        self.root.resizable(0, 0)
         self.root.title("SuperTicTacToe")
         self.root.geometry(geometria)
         self.frames = {} # tous les ojets graphiques de frames avec en cle la position dans le tableau (1,1) , (1,2) ...
@@ -11,18 +12,24 @@ class Jeu:
         self.croix = [] #positions avec croix
         self.rond = [] #positions ou il y a un rond
         self.positionspossibles = [(i,j) for i in range(3) for j in range(3) if i+j<=4]
+        self.nseo = ["NW", "N", "NE", "W", None, "E", "SW", "S", "SE"]
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(1, weight=1)
+        self.root.columnconfigure(2, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.root.rowconfigure(1, weight=1)
+        self.root.rowconfigure(2, weight=1)
         self.initialisationgraph()
 
 
 
 
-    def initialisationgraph(self): #creer frames pour chaque garnd carré puis grid
+    def initialisationgraph(self): #creer frames pour chaque garnd carré puis grid et ajouter tous les tiktaktoes
         for p in range(len(self.positionspossibles)):
             f = Frame(self.root, background="white")
             self.tableau[p] = {}
             self.frames[p] = f
-            f.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1])
-        print(self.frames)
+            f.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], sticky=self.nseo[p], padx=0, pady=0, highlightbackground="blue", highlightthickness=1)
 
         for h in range(9):
             self.creertictac(h)
@@ -30,7 +37,7 @@ class Jeu:
     def creertictac(self, numpos):
         for p in range(len(self.positionspossibles)):
             b = Button(self.frames[numpos], borderwidth = 0, background="white", foreground="black")
-            b.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1])
+            b.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], padx=0, pady=0)
             self.obj[numpos].append((p, b))
 
 
