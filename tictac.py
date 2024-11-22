85a5cff56f809860b417b6f75dcf37a144c117d8
SixEaux <dc200421@gmail.com>
2024-11-22 10:51:35 +0100

    branche principale

--- tictac.py
+++ tictac.py
@@ -3,57 +3,58 @@ from tkinter import *
 
 class Jeu:
     def __init__(self, root, geometria):
-
         self.root = root
         self.root.resizable(0, 0)
         self.root.title("SuperTicTacToe")
         self.root.geometry(geometria)
-        self.frames = {} # tous les ojets graphiques de frames avec en cle la position dans le tableau (1,1) , (1,2) ...
+        self.geometry = (geometria[:3], geometria[4:])
+
         self.tableau = {} # code du tableau c'est 11,12,13,14...
-        self.obj = {i : [] for i in range(9)}
-        self.croix = [] #positions avec croix
-        self.rond = [] #positions ou il y a un rond
+
         self.positionspossibles = [(i,j) for i in range(3) for j in range(3) if i+j<=4]
         self.nseo = ["NW", "N", "NE", "W", None, "E", "SW", "S", "SE"]
+
+
         self.root.columnconfigure(0, weight=1)
         self.root.columnconfigure(1, weight=1)
         self.root.columnconfigure(2, weight=1)
         self.root.rowconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
         self.root.rowconfigure(2, weight=1)
+
+        print(dir(self.root))
+
         self.initialisationgraph()
-
-
-
-
 
     def initialisationgraph(self): #creer frames pour chaque garnd carré puis grid et ajouter tous les tiktaktoes
         for p in range(len(self.positionspossibles)):
             f = Frame(self.root, background="white")
-            self.tableau[p] = {}
-            self.frames[p] = f
+            self.tableau[p] = [None for i in range(9)]
+
             f.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], sticky=self.nseo[p], padx=0, pady=0) #, highlightbackground="blue", highlightthickness=1
 
-        for h in range(9):
+        for h in self.root.winfo_children():
             self.creertictac(h)
 
-    def creertictac(self, numpos):
+    def creertictac(self, frame):
         for p in range(len(self.positionspossibles)):
-            b = Button(self.frames[numpos], borderwidth = 1, background="white", foreground="black")
-            b.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], padx=0, pady=0, ipadx = 0, ipady = 0)
-            b = Button(self.frames[numpos], borderwidth = 1, background="white", foreground="black")
+            b = Button(frame, borderwidth = 1, background="white", foreground="black")
             b.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], padx=0, pady=0)
 
             height=round(int(self.geometry[0])/83)/2
             width=round(int(self.geometry[0])/83)
-            print(height, width)
-            b.config( height =int(height), width =int(width) )
 
-            self.obj[numpos].append((p, b))
+            b.config( height =int(height), width =int(width))
 
 
-    def ptitictac(self):
-        pass
+
+    def testgagne(self, ):
+        gagne = {(0,1,2), (3,4,5), (6,7,8), (0,3,6), }
+
+
+    def gagnegrosframe(self, frame):
+        for obj in frame.winfo_children():
+            obj.destroy()
 
     def placement(self, figure, pos):
         pass
@@ -62,6 +63,8 @@ class Jeu:
         pass
 
 
+
+
 root = Tk()
-g = Jeu(root, "450x450")
+g = Jeu(root, "500x500")
 root.mainloop()
