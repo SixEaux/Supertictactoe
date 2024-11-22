be80add0cbb2e7e4725964efd7296f5cc2a4a2db
SixEaux <dc200421@gmail.com>
2024-11-22 15:28:33 +0100

    branche principale

--- tictac.py
+++ tictac.py
@@ -7,20 +7,19 @@ class Jeu:
         self.root.resizable(0, 0)
         self.root.title("SuperTicTacToe")
         self.root.geometry(geometria)
-        self.geometry = (geometria[:3], geometria[4:])
+        self.geometry = tuple(map(int, geometria.split("x")))
 
         self.tableau = {} # code du tableau c'est 11,12,13,14...
 
         self.positionspossibles = [(i,j) for i in range(3) for j in range(3) if i+j<=4]
         self.nseo = ["NW", "N", "NE", "W", None, "E", "SW", "S", "SE"]
+        self.postotab = {(0,0):0, (0,1):1, (0,2):2, (1,0):3, (1,1):4, (1,2): 5, (2,0):6, (2,1):7, (2,2):8}
 
-
-        self.root.columnconfigure(0, weight=1)
-        self.root.columnconfigure(1, weight=1)
-        self.root.columnconfigure(2, weight=1)
-        self.root.rowconfigure(0, weight=1)
-        self.root.rowconfigure(1, weight=1)
-        self.root.rowconfigure(2, weight=1)
+        for i in range(3):
+            self.root.columnconfigure(i, weight=1)
+            self.root.rowconfigure(i, weight=1)
+        self.root.columnconfigure(3, weight=3)
+        self.root.rowconfigure(3, weight=1)
 
         print(dir(self.root))
 
@@ -31,15 +30,21 @@ class Jeu:
             f = Frame(self.root, background="white")
             self.tableau[p] = [None for i in range(9)]
 
-            f.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], sticky=self.nseo[p], padx=0, pady=0) #, highlightbackground="blue", highlightthickness=1
+            f.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], sticky=self.nseo[p], ipadx=2, ipady=2)
+
+        # Fran = Frame(self.root, background="white")
+        # Fran.grid(column=3)
+        # Label(Fran, text= "TIKTAKTOE")
 
         for h in self.root.winfo_children():
             self.creertictac(h)
 
+
+
     def creertictac(self, frame):
         for p in range(len(self.positionspossibles)):
             b = Button(frame, borderwidth = 1, background="white", foreground="black")
-            b.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], padx=0, pady=0)
+            b.grid(row=self.positionspossibles[p][0], column=self.positionspossibles[p][1], padx=1, pady=1)
 
             height=round(int(self.geometry[0])/83)/2
             width=round(int(self.geometry[0])/83)
@@ -48,8 +53,14 @@ class Jeu:
 
 
 
-    def testgagne(self, ):
-        gagne = {(0,1,2), (3,4,5), (6,7,8), (0,3,6), }
+    def testgagne(self, dernbttouch):
+        gagne = {(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2, 5, 8), (0,4,8), (2,4,6)}
+        frm = self.root.winfo_parent(dernbttouch)
+        frmij = self.root.grid_location(frm)
+        t = self.postotab[(frmij[0], frmij[1])]
+        for i in gagne:
+            if self.tableau[t][i[0]] == self.tableau[t][i[1]] == self.tableau[t][i[2]]:
+                self.gagnegrosframe(frm)
 
 
     def gagnegrosframe(self, frame):
@@ -66,5 +77,5 @@ class Jeu:
 
 
 root = Tk()
-g = Jeu(root, "500x500")
+g = Jeu(root, "600x600")
 root.mainloop()
