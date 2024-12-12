ee6641776429548acfd2fe8c76d24eec0c9ee314
SixEaux <dc200421@gmail.com>
2024-12-12 21:42:35 +0100

    branche principale

--- test file.py
+++
@@ -1,8 +0,0 @@
-position = ["X", "O", "",
-            "", "X", "O",
-            "", "", ""]
-
-# Appel de l'algorithme
-a =
-resultat = self.minimaxpetit(position, 3, self.ordijoue)
-print("Case choisie :", resultat[0], "Score :", resultat[1])
